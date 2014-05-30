70bc84198694cfefb3a183da8bd454eddb5098ba
Josh Comeau <joshwcomeau@gmail.com>
2014-05-30 17:26:15 -0400

    Created some ugly code to live-update a sorted list based on user input. Will be refactored when I know more Angular.

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,14 @@ var app = angular.module("sortCircuit", []);
 app.controller('SelectionController', ['$scope', function($scope) {
   $scope.sort_list = function() {
     arr = $scope.list.replace(/ /g, '').split(",");
-
     $scope.sorted_list = selection_sort(arr);
+    $scope.sorted_object = {};
+    _.each($scope.sorted_list, function(element, index) {
+      if ( parseInt(element) ) {
+        $scope.sorted_object[index] = element;
+        console.log($scope.sorted_object);
+      }
+    });
+
   };
 }]);
