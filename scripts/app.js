7fd6807bd7c9c4d16d016720e514e9c36ae6fd30
Josh Comeau <joshwcomeau@gmail.com>
2014-05-30 17:34:02 -0400

    Changed the structure of the sorted array to allow for lists with more than 10 items

--- scripts/app.js
+++ scripts/app.js
@@ -4,11 +4,14 @@ app.controller('SelectionController', ['$scope', function($scope) {
   $scope.sort_list = function() {
     arr = $scope.list.replace(/ /g, '').split(",");
     $scope.sorted_list = selection_sort(arr);
-    $scope.sorted_object = {};
+    $scope.sorted_objects = [];
     _.each($scope.sorted_list, function(element, index) {
       if ( parseInt(element) ) {
-        $scope.sorted_object[index] = element;
-        console.log($scope.sorted_object);
+        $scope.sorted_objects.push({
+          position: index,
+          number: element
+        });
+        
       }
     });
 
